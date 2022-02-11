fbe6b0214101280c808c7979d0db3e3343d4f427
Manuel Aguirre <maguirre@optimeconsulting.com>
2022-02-11 16:07:39 -0500

    Creando primer php

---
+++ Optime/Email/Bundle/OptimeEmailBundle.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author Manuel Aguirre
+ */
+
+namespace Optime\Email\Bundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Manuel Aguirre
+ */
+class OptimeEmailBundle extends Bundle
+{
+
+}
